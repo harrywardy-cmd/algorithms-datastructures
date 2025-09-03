ba8fc6f165c4bce0d2098ec3fb0dd727c53928f5
harrywardy-cmd <harrywardy303@gmail.com>
2025-09-03 19:53:41 +1000

    Update buy_and_sell_stock.py

--- arrays/buy_and_sell_stock.py
+++ arrays/buy_and_sell_stock.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxP = 0
-        minBuy = prices[0]
+        maxP = 0                                        #current max profit 
+        minBuy = prices[0]               
 
         for sell in prices:
-            maxP = max(maxP, sell - minBuy)
-            minBuy = min(minBuy, sell)
-        return maxP
+            maxP = max(maxP, sell - minBuy)            #find the max between max profit(maxP) stored outside the for loop and the current sell-minBuy
+            minBuy = min(minBuy, sell)                 #check if the sell is smaller then the minBuy outside the loop and will update it
+        return maxP                                    #return the minBuy, if the minBuy is greater then the sell the maxP will never be updated and return 0
