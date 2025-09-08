7f69a179036d41a68bfd979f07a45742b17a75ad
harrywardy-cmd <harrywardy303@gmail.com>
2025-09-08 19:41:22 +1000

    Update valid_anagram.py

--- arrays/valid_anagram.py
+++ arrays/valid_anagram.py
@@ -1,11 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        if len(s) != len(t): # both strings will have to be the same length 
             return False
 
-        countS, countT = {}, {}
+        countS, countT = {}, {} # creates hashmap
 
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countS[s[i]] = 1 + countS.get(s[i], 0)  # add the letters to the hashmap as the key and the frequency as the value
             countT[t[i]] = 1 + countT.get(t[i], 0)
-        return countS == countT
+        return countS == countT #this will compare each hashmap
