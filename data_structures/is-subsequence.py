beb143e59f84ca52a22e79fbfb2551430a733ee6
Harry Ward <harrywardy303@gmail.com>
2026-06-25 20:14:46 +1000

    Update is-subsequence.py

--- data_structures/is-subsequence.py
+++ data_structures/is-subsequence.py
@@ -1,11 +1,22 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        i,j = 0,0
+        # Pointer for string t
+        i = 0
 
+        # Pointer for string s (the subsequence we're trying to match)
+        j = 0
+
+        # Traverse through both strings
         while i < len(t) and j < len(s):
+
+            # If the current characters match,
+            # move to the next character in s
             if t[i] == s[j]:
                 j += 1
+
+            # Always move to the next character in t
             i += 1
 
-
+        # If we've matched every character in s,
+        # then s is a subsequence of t
         return j == len(s)
