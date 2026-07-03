2cd9dc6c89994963824c354e091f3ffc28a7a61e
Harry Ward <harrywardy303@gmail.com>
2026-07-03 20:01:01 +1000

    Update number-of-students-unable-to-eat-lunch.py

--- data_structures/Linked-Lists/number-of-students-unable-to-eat-lunch.py
+++ data_structures/Linked-Lists/number-of-students-unable-to-eat-lunch.py
@@ -1,18 +1,39 @@
+from collections import deque
+
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        n = len(students)
+        # Store all students in a queue so they can move from the
+        # front to the back if they don't want the current sandwich.
         q = deque(students)
 
-        res = n
-        for sandwitch in sandwiches:    #will itterate through the types of sandwitches
+        # Total number of students currently without a sandwich.
+        res = len(students)
+
+        # Process each sandwich from the top of the sandwich stack.
+        for sandwich in sandwiches:
+            # Count how many students have been rotated.
+            # If every remaining student has been checked and none
+            # want the current sandwich, no further sandwiches can
+            # be eaten.
             count = 0
-            while count < n and q[0] != sandwitch:  #if the studend doesnt like the sandwitch the will move to the back of the queue
-                cur = q.popleft()
-                q.append(cur)
+
+            # Keep rotating students until someone wants the sandwich
+            # or every remaining student has been checked.
+            while count < len(q) and q[0] != sandwich:
+                student = q.popleft()   # Remove student from the front.
+                q.append(student)       # Move them to the back.
                 count += 1
-            if q[0] == sandwitch:                 #if they like the sandwitch they will be popped from the queue and n (the total amount of students with out a sandwitch) will decrease
+
+            # If the student at the front likes the sandwich,
+            # they take it and leave the queue.
+            if q and q[0] == sandwich:
                 q.popleft()
                 res -= 1
             else:
+                # No student wants the current sandwich.
+                # Since the sandwich order cannot change,
+                # no more students can eat.
                 break
-        return res                    #return the total amount of students without a sandwitch
+
+        # Return the number of students who could not eat.
+        return res
