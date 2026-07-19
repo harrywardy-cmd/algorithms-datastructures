797ec98388d54fc44a7b03765b3269e39e66c7f5
Harry Ward <harrywardy303@gmail.com>
2026-07-19 19:49:58 +1000

    Update concatenation_of_array.py

--- data_structures/arrays/concatenation_of_array.py
+++ data_structures/arrays/concatenation_of_array.py
@@ -1,7 +1,13 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
+        # Stores the concatenated result.
         ans = []
+
+        # Repeat the process twice to create nums + nums.
         for i in range(2):
+            # Append every element from the original array.
             for num in nums:
                 ans.append(num)
+
+        # Return the concatenated array.
         return ans
